7ca7ee08f4e64ad2d8ea7a8c08e74db09c170d73
Rahul Bora <rahubora11@gmail.com>
2021-03-09 13:14:35 +0530

    files

---
+++ index.html.js
@@ -0,0 +1,419 @@
+let html =(orderId, date, deliverydate, name,price)=>`<body style='background-color: white;' >
+    <img src="https://images-eu.ssl-images-amazon.com/images/G/31/x-locale/common/amazon-logo-tiny._CB485933241_.gif" width="91" height="26" border="0" align="left">    
+    <br>
+    <br clear="\&quot;all\&quot;">
+      <center><b class="h1">
+                Final Details for #${orderId}
+      </b><br>
+    
+    </center><br>
+    
+    <table width="90%" cellspacing="0" cellpadding="0" border="0" bgcolor="#ffffff" align="center">
+      <tbody><tr>
+        <td>    
+          <table width="100%" cellspacing="0" cellpadding="0" border="0" align="center">
+            <tbody><tr><td valign="top" align="left">
+              <b>
+                    Order Placed:
+              </b>
+              ${date}
+            </td></tr>    
+            <tr><td valign="top" align="left">
+              <b>Amazon.in order number:</b>
+               ${orderId}
+            </td></tr>
+    
+    
+            <tr><td valign="top" align="left">
+              <b>Order Total:
+              <span style="text-decoration: inherit; white-space: nowrap;"><span class="currencyINR">&nbsp;&nbsp;</span><span class="currencyINRFallback" style="display:none">Rs. </span>${price}</span></b>
+            </td></tr>
+    
+    
+          </tbody></table>
+          <br>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#000000" align="center">
+      <tbody><tr bgcolor="#000000">
+        <td>
+          <table width="100%" cellspacing="3" cellpadding="0" border="0" bgcolor="#000000" align="center">
+            <tbody><tr bgcolor="#ffffff">
+              <td colspan="2" valign="top">
+                <table width="100%" cellspacing="0" cellpadding="5" border="0">
+                  <tbody><tr bgcolor="#ffffff">
+                    <td>
+                      <b class="sans"><center>
+                      Dispatched on ${deliverydate}
+                      </center></b>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2" valign="top" bgcolor="#ffffff">
+                <table width="100%" cellspacing="0" cellpadding="2" border="0">
+                  <tbody><tr valign="top">
+                    <td width="100%">
+                      <table cellspacing="0" cellpadding="2" border="0" align="right">
+                        <tbody><tr valign="top">
+                          <td align="right">
+                            &nbsp;
+                          </td>
+                        </tr>
+                      </tbody></table>
+                      <table width="100%" cellspacing="2" cellpadding="0" border="0">
+                        <tbody><tr valign="top">
+                          <td valign="top">
+                            <b>Items Ordered</b>
+                          </td>
+                          <td valign="top" align="right">
+                            <b>Price</b>
+                          </td>
+                        </tr>
+                        
+    
+    
+    
+    
+      <tr>
+      <input type="hidden" name="jnljoxmpromnooy" value="1">
+      <td colspan="1" valign="top">
+        
+        1
+    
+        of:
+    
+        <i>${name}</i><br>
+    
+  
+        
+      </td>
+      <td colspan="2" valign="top" align="right">
+        
+        <span style="text-decoration: inherit; white-space: nowrap;"><span class="currencyINR">&nbsp;&nbsp;</span><span class="currencyINRFallback" style="display:none">Rs. </span>${price}</span><br>
+        
+      </td></tr>
+    
+    
+    
+    
+    
+      
+    
+    
+    
+    
+    
+    
+                      </tbody></table>
+                      <br>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2" valign="top" bgcolor="#ffffff">
+                <table width="100%" cellspacing="0" cellpadding="2" border="0">
+                  <tbody><tr>
+                    <td>
+                      
+    
+    <b>
+    Delivery Address:
+    </b>
+    
+    
+    
+    <br>
+    
+    
+    
+    <div class="displayAddressDiv">
+    <ul class="displayAddressUL" style="list-style-type: none" >
+    <li class="displayAddressLI displayAddressFullName">Rahul Bora</li>
+    <li class="displayAddressLI displayAddressAddressLine1">203, Marvel Residency, Proms Complex</li>
+    <li class="displayAddressLI displayAddressAddressLine2">SBI Colony , Koramangala</li>
+    <li class="displayAddressLI displayAddressCityStateOrRegionPostalCode">BENGALURU, KARNATAKA 560034</li>
+    <li class="displayAddressLI displayAddressCountryName">India</li>
+    </ul>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                        
+    
+    
+    <br><b>
+    Delivery Option:
+    </b>
+    
+    
+    
+    <br>
+    Standard Delivery
+    <br>
+    
+    
+    
+    
+    
+                        
+    
+    
+    
+                    </td>
+                    <td align="right">
+                      <table cellspacing="1" cellpadding="0" border="0">
+    </table>
+    
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+          </tbody></table>
+        </td>
+      </tr>
+    </tbody></table>
+    
+    <br>
+    
+    
+    
+    
+    <table width="100%" cellspacing="0" cellpadding="0" border="0" bgcolor="#000000" align="center">
+      <tbody><tr bgcolor="#000000">
+        <td>
+          <table width="100%" cellspacing="3" cellpadding="0" border="0" bgcolor="#000000" align="center">
+            <tbody><tr bgcolor="#ffffff">
+              <td colspan="2" valign="top">
+                <table width="100%" cellspacing="0" cellpadding="5" border="0">
+                  <tbody><tr bgcolor="#ffffff">
+                    <td>
+                      <b class="sans"><center>Payment information</center></b>
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+            <tr>
+              <td colspan="2" valign="top" bgcolor="#ffffff">
+                <table width="100%" cellspacing="0" cellpadding="2" border="0">
+                  <tbody><tr valign="top">
+                    <td width="100%">
+                      <table cellspacing="0" cellpadding="2" border="0" align="right">
+                        <tbody><tr valign="top">
+                          <td align="right">
+                            <table cellspacing="1" cellpadding="0" border="0">
+        
+        <tbody><tr valign="top">
+            <td nowrap="nowrap" align="right">Item(s) Subtotal:</td>
+            <td nowrap="nowrap" align="right"><span style="text-decoration: inherit; white-space: nowrap;"><span class="currencyINR">&nbsp;&nbsp;</span><span class="currencyINRFallback" style="display:none">Rs. </span>${price}</span></td>
+        </tr> 
+        
+   
+        <tr valign="top">
+            <td nowrap="nowrap" align="right">&nbsp;</td>
+            <td nowrap="nowrap" align="right">-----</td>
+        </tr> 
+        
+        <tr valign="top">
+            <td nowrap="nowrap" align="right">Total:</td>
+            <td nowrap="nowrap" align="right"><span style="text-decoration: inherit; white-space: nowrap;"><span class="currencyINR">&nbsp;&nbsp;</span><span class="currencyINRFallback" style="display:none">Rs. </span>${price}</span></td>
+        </tr> 
+        
+        <tr valign="top">
+            <td nowrap="nowrap" align="right">&nbsp;</td>
+            <td nowrap="nowrap" align="right">-----</td>
+        </tr> 
+        
+        <tr valign="top">
+            <td nowrap="nowrap" align="right"><b>Grand Total:</b></td>
+            <td nowrap="nowrap" align="right"><b><span style="text-decoration: inherit; white-space: nowrap;"><span class="currencyINR">&nbsp;&nbsp;</span><span class="currencyINRFallback" style="display:none">Rs. </span>${price}</span></b></td>
+        </tr> 
+    </tbody></table>
+    
+                          </td>
+                        </tr>
+                      </tbody></table>
+    
+                      
+    
+    
+    
+    
+    <b>Payment Method: </b>
+    
+    
+    <br>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      BHIM UPI
+    <br> 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+                      
+    
+    <br>
+    <b>Billing Address:</b>
+    
+    
+    
+    
+    
+    
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <div class="displayAddressDiv">
+    <ul class="displayAddressUL" style="list-style-type:none" >
+    <li class="displayAddressLI displayAddressFullName">Rahul Bora</li>
+    <li class="displayAddressLI displayAddressAddressLine1">203, Marvel Residency, Proms Complex</li>
+    <li class="displayAddressLI displayAddressAddressLine2">SBI Colony , Koramangala</li>
+    <li class="displayAddressLI displayAddressCityStateOrRegionPostalCode">BENGALURU, KARNATAKA 560034</li>
+    <li class="displayAddressLI displayAddressCountryName">India</li>
+    </ul>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                    </td>
+                  </tr>
+                </tbody></table>
+              </td>
+            </tr>
+          </tbody></table>
+        </td>
+      </tr>
+    </tbody></table>
+    
+    
+    
+        </td>
+      </tr>
+    </tbody></table>
+    
+    <center>
+    
+      <p>To view the status of your order, return to <a href="/gp/css/summary/edit.html?ie=UTF8&amp;orderID=408-2678637-9242717">Order Summary</a>.</p>
+    
+      <p><b>Please note:</b> this is not a GST invoice.</p>
+    </center>
+    
+    
+    
+    
+    
+    <script>   if (typeof uet == 'function') { uet('cf'); } </script> 
+    <br>
+    
+    
+    
+    
+    
+    <table width="100%" align="center">
+    
+    
+    
+    <tbody><tr align="center"><td colspan="2">
+    <font size="-2">
+    
+    
+    
+    
+    
+    
+    
+    <a href="http://www.amazon.in/gp/help/customer/display.html/ref=cs_nav_bn_2_1?ie=UTF8&amp;nodeId=200522590" rel="nofollow">Conditions of Use &amp; Sale</a>&nbsp;|&nbsp;
+    
+    
+    
+    
+    
+    
+    <a href="http://www.amazon.in/gp/help/customer/display.html?_encoding=UTF8&amp;nodeId=200522700" rel="nofollow">Privacy Notice</a>&nbsp;|&nbsp;
+    
+    
+    
+    
+    
+    
+    <a href="http://www.amazon.in/gp/help/customer/display.html?ie=UTF8&amp;nodeId=200571750" rel="nofollow">Legal Notice</a>&nbsp;&nbsp;© 2012-2020, Amazon.com, Inc. and its affiliates</font>
+    </td></tr>
+    <!-- whfh-Kec18BNhJ6+LlXe8+ZYiXQeSh2uKNGmHuBqaXXL/eBa/joS14HDfLg== rid-6G2GF0CG2Q76PCVHQBAG -->
+    </tbody></table>
+    </body>`
+
+
+    module.exports = html;
